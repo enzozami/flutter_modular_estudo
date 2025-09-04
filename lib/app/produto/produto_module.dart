d8c922cdfad3ac86cea933e1c2c2a72715d588d8
Enzo Zamineli <enzozamineli@gmail.com>
2025-09-04 13:41:37 -0300

    controle e injecao de depencencias parte 2

--- lib/app/produto/produto_module.dart
+++ lib/app/produto/produto_module.dart
@@ -1,9 +1,15 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_modular_example/app/categoria/categoria_module.dart';
 import 'package:flutter_modular_example/app/produto/produto_page.dart';
 
 class ProdutoModule extends Module {
   @override
   List<Bind> get binds => [];
+
+  @override
+  List<Module> get imports => [
+        CategoriaModule(),
+      ];
 
   @override
   List<ModularRoute> get routes => [
